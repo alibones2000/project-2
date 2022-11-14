829bbeb2fe40af8667f5583801d8e26e0311ee46
Abena Adu <abeanadu@gmail.com>
2022-11-14 16:51:49 -0500

    Updated MovieCard

--- src/MovieCard.js
+++ src/MovieCard.js
@@ -1,15 +1,19 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 function MovieCard({film}) {
+const [originalTitle, setOriginalTitle] = useState (true)
 const {title, original_title, original_title_romanised, image, description, director, release_date} = film
 
+function handleClick () {
+  setOriginalTitle((originalTitle) => !originalTitle)
+}
   return (
     <li >
       <div >
-        <img src={image} alt={title}/>
+        <img onClick={handleClick} src={image} alt={title}/>
         <div >
           <h1>{title}</h1>
-          <h2>Original Title</h2>
+          <h2>{originalTitle ? original_title : original_title_romanised}</h2>
           <p>{description}</p>
           <p>{director}</p>
           <p>{release_date} </p>
